bcefc0f06e774964d74017927f3d348147c96754
viyeumaden <66916286+viyeumaden@users.noreply.github.com>
2023-03-29 15:03:27 +0700

    x

---
+++ src/main/java/Student.java
@@ -0,0 +1,16 @@
+public class Student {
+    public String name ="John";
+    public String classes="C02";
+    //cons
+    Student(){
+
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setClasses(String classes) {
+        this.classes = classes;
+    }
+}
---
+++ src/main/java/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+    Student s1= new Student();
+    Student s2=new Student();
+    s1.setName("anh");
+    s1.setClasses("C03");
+        System.out.println(s1.toString());
+    }
+}
